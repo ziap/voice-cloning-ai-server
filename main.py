d3dabf875ca28c779a6f95d15626df8e9f096f88
Zap <67962871+ziap@users.noreply.github.com>
2022-07-16 21:56:56 +0700

    Move config.py into main.py

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from fastapi import FastAPI
 from routes import router
 
 import uvicorn
-import config
 
+from dotenv import load_dotenv
+from os import environ as env
+
+load_dotenv()
+
+HOST = env.get('HOST', '127.0.0.1')
+PORT = int(env.get('PORT', 3000))
+DEBUG = bool(env.get('DEBUG', False))
 
 app = FastAPI()
 app.include_router(router)
 
 
 if __name__ == '__main__':
-    if config.DEBUG:
-        uvicorn.run('main:app', host=config.HOST, port=config.PORT, reload=True)
+    if DEBUG:
+        uvicorn.run('main:app', host=HOST, port=PORT, reload=True)
     else:
-        uvicorn.run(app, host=config.HOST, port=config.PORT)
+        uvicorn.run(app, host=HOST, port=PORT)
 
