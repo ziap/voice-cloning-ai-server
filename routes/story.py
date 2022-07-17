69c87ebbd03ffbea928ce27d47d1b8bc9a0f7283
Zap <67962871+ziap@users.noreply.github.com>
2022-07-17 15:05:54 +0700

    Finish story routes

--- routes/story.py
+++ routes/story.py
@@ -1,7 +1,61 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from pydantic import BaseModel
+from glob import glob
+
+import os
+import aiofiles
+
+class Story(BaseModel):
+    title: str
+    content: str
+
+root_data_path = os.path.join(os.getcwd(), 'data/stories')
+if not os.path.isdir(root_data_path):
+    os.makedirs(root_data_path)
 
 router = APIRouter(
     prefix='/story',
     tags=['Story'],
     responses={404: {"description": "Not found"}},
 )
+
+def delete_story(storyid):
+    deleted = False
+    story_glob = StoryGlob(storyid)
+    if len(story_glob) > 0:
+        deleted = True
+        for file in story_glob:
+            if file[file.rindex('/') + 1:file.rindex('-')] == storyid:
+                os.remove(file)
+    return deleted
+
+def StoryGlob(storyid):
+    return glob(os.path.join(root_data_path, "%s-*.txt" % storyid))
+
+@router.get("-{storyid}", status_code=404)
+async def GetStory(storyid, response: Response):
+    story_glob = StoryGlob(storyid)
+    if len(story_glob) > 0:
+        file = story_glob[0]
+        if file[file.rindex('/') + 1:file.rindex('-')] != storyid:
+            return
+        response.status_code = 200
+        title = file[file.rindex('-') + 1:file.rindex('.')]
+        async with aiofiles.open(file, mode='r') as f:
+            content = await f.read()
+        return {'id': storyid, 'title': title, 'content': content} 
+
+@router.put("-{storyid}", status_code=201)
+async def PutStory(storyid, story: Story):
+    delete_story(storyid)
+    file = os.path.join(root_data_path, "%s-%s.txt" % (storyid, story.title))
+    async with aiofiles.open(file, mode='w') as f:
+       await f.write(story.content) 
+    return {'id': storyid}
+
+@router.delete("-{storyid}", status_code=409)
+async def DeleteStory(storyid, response: Response):
+    deleted = delete_story(storyid)
+    if deleted:
+        response.status_code = 200
+    return {'id': storyid, 'deleted': deleted}
--- routes/user.py
+++ routes/user.py
@@ -8,20 +8,16 @@ import librosa
 
 from loadmodel import preprocess_wav, embed_utterance
 
-
 root_data_path = os.path.join(os.getcwd(), 'data/users')
 if not os.path.isdir(root_data_path):
     os.makedirs(root_data_path)
-
 
 encode_queue_path = os.path.join(os.getcwd(), 'data/encode-queue')
 if not os.path.isdir(encode_queue_path):
     os.makedirs(encode_queue_path)
 
-
 def UserPath(userid):
     return os.path.join(root_data_path, userid)
-
 
 def encode_voice(userid):
     # Get files
@@ -38,9 +34,7 @@ def encode_voice(userid):
     # Save output
     if not os.path.isdir(user_path):
         os.makedirs(user_path)
-    
     np.save(out_file, data)
-
 
 router = APIRouter(
     prefix='/user',
@@ -48,13 +42,11 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-
 @router.get('-{userid}', status_code=404)
 async def GetUser(userid, response: Response):
     if os.path.isdir(UserPath(userid)):
         response.status_code = 200
         return {'id': userid}
-
 
 @router.put('-{userid}', status_code=201)
 async def PutUser(userid, tasks: BackgroundTasks, voice: UploadFile = File(...)):
@@ -64,16 +56,14 @@ async def PutUser(userid, tasks: BackgroundTasks, voice: UploadFile = File(...))
         while content:
             await out_file.write(content)
             content = await voice.read(1024)
-    
     tasks.add_task(encode_voice, userid)
     return {'id': userid, 'file': voice_queue_file}
-
 
 @router.delete('-{userid}', status_code=409)
 async def DeleteUser(userid, response: Response):
     user_path = UserPath(userid)
-    
     if os.path.isdir(UserPath(user_path)):
         response.status_code=200
         rmtree(user_path)
         return {'id': userid, 'deleted': True}
+    return {'id': userid, 'deleted': False}
