e5744fadbb34a5977f0a9e79cf72ea3eefc92940
Zap <67962871+ziap@users.noreply.github.com>
2022-07-17 13:09:01 +0700

    Finish user route

---
+++ loadmodel.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.append("model")
+
+from pathlib import Path
+
+from encoder.params_model import model_embedding_size as speaker_embedding_size
+from encoder import inference as encoder
+from synthesizer.inference import Synthesizer
+from utils.default_models import ensure_default_models
+from vocoder import inference as vocoder
+
+import numpy as np
+
+model_path = Path("saved_models")
+
+print("Preparing the encoder, the synthesizer and the vocoder...")
+ensure_default_models(model_path)
+encoder.load_model(Path("saved_models/default/encoder.pt"))
+synthesizer = Synthesizer(Path("saved_models/default/synthesizer.pt"))
+vocoder.load_model("saved_models/default/vocoder.pt")
+
+# Export the models
+preprocess_wav = encoder.preprocess_wav
+embed_utterance = encoder.embed_utterance
+synthesize_spectrograms = synthesizer.synthesize_spectrograms
+infer_waveform = vocoder.infer_waveform
+
+# Indent not to export test variables
+if True:
+    print("\tTesting the encoder...")
+    embed_utterance(np.zeros(encoder.sampling_rate))
+
+    embed = np.random.rand(speaker_embedding_size)
+    embed /= np.linalg.norm(embed)
+    embeds = [embed, np.zeros(speaker_embedding_size)]
+    texts = ["test 1", "test 2"]
+
+    print("\tTesting the synthesizer... (loading the model will output a lot of text)")
+    mels = synthesize_spectrograms(texts, embeds)
+
+    mel = np.concatenate(mels, axis=1)
+
+    print("\tTesting the vocoder...")
+    infer_waveform(mel, target=200, overlap=50)
+
+    print("All test passed! You can now synthesize speech.\n\n")
--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, File, UploadFile, BackgroundTasks
+from shutil import rmtree
+ 
+import numpy as np
+import os
+import aiofiles
+import librosa
+
+from loadmodel import preprocess_wav, embed_utterance
+
+
+root_data_path = os.path.join(os.getcwd(), 'data/users')
+if not os.path.isdir(root_data_path):
+    os.makedirs(root_data_path)
+
+
+encode_queue_path = os.path.join(os.getcwd(), 'data/encode-queue')
+if not os.path.isdir(encode_queue_path):
+    os.makedirs(encode_queue_path)
+
+
+def UserPath(userid):
+    return os.path.join(root_data_path, userid)
+
+
+def encode_voice(userid):
+    # Get files
+    in_file = os.path.join(encode_queue_path, '%s.wav' % userid)
+    user_path = UserPath(userid)
+    out_file = os.path.join(user_path, 'voice')
+    
+    # Generate embedding vector
+    original_wav, sampling_rate = librosa.load(in_file)
+    os.remove(in_file)
+    preprocessed = preprocess_wav(original_wav, sampling_rate)
+    data = embed_utterance(preprocessed)
+
+    # Save output
+    if not os.path.isdir(user_path):
+        os.makedirs(user_path)
+    
+    np.save(out_file, data)
+
 
 router = APIRouter(
     prefix='/user',
@@ -6,6 +48,32 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get('-{userid}')
-def getuser(userid):
-    return {'id': userid}
+
+@router.get('-{userid}', status_code=404)
+async def GetUser(userid, response: Response):
+    if os.path.isdir(UserPath(userid)):
+        response.status_code = 200
+        return {'id': userid}
+
+
+@router.put('-{userid}', status_code=201)
+async def PutUser(userid, tasks: BackgroundTasks, voice: UploadFile = File(...)):
+    voice_queue_file = os.path.join(encode_queue_path, '%s.wav' % userid) 
+    async with aiofiles.open(voice_queue_file, 'wb') as out_file:
+        content = await voice.read(1024)
+        while content:
+            await out_file.write(content)
+            content = await voice.read(1024)
+    
+    tasks.add_task(encode_voice, userid)
+    return {'id': userid, 'file': voice_queue_file}
+
+
+@router.delete('-{userid}', status_code=409)
+async def DeleteUser(userid, response: Response):
+    user_path = UserPath(userid)
+    
+    if os.path.isdir(UserPath(user_path)):
+        response.status_code=200
+        rmtree(user_path)
+        return {'id': userid, 'deleted': True}
